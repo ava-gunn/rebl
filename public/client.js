313d42103a29fc1a7b9ed49355fecca8336a188d
Ava Gunn <ava.gunn@pm.me>
2025-10-01 15:27:19 -0400

    Add aliases

--- public/client.js
+++ public/client.js
@@ -1,3 +1,20 @@
+// Console Utilities API
+window.$ = document.querySelector.bind(document);
+window.$$ = document.querySelectorAll.bind(document);
+window.clear = console.clear.bind(console);
+window.copy = (object) => {
+  if (typeof object !== 'string') {
+    object = JSON.stringify(object, null, 2);
+  }
+  navigator.clipboard.writeText(object)
+    .then(() => console.log('Copied to clipboard.'))
+    .catch(err => console.error('Failed to copy: ', err));
+};
+window.inspect = console.dir.bind(console);
+window.keys = Object.keys.bind(Object);
+window.values = Object.values.bind(Object);
+window.table = console.table.bind(console);
+
 const socket = new WebSocket("ws://localhost:8080/ws/browser");
 
 socket.addEventListener("open", () => {
